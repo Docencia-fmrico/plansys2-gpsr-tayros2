3b365bfd7d360f7c44f4b7455aa9ac6a956c0d2e
SaulN99 <saulnavajas@gmail.com>
2023-04-24 20:25:09 +0200

    Transport bt works

--- src/gpsr_controller_node.cpp
+++ src/gpsr_controller_node.cpp
@@ -98,7 +98,7 @@ public:
     problem_expert_->addPredicate(plansys2::Predicate("(close doorB)"));
     problem_expert_->addPredicate(plansys2::Predicate("(close doorC)"));
     problem_expert_->addPredicate(plansys2::Predicate("(close doorD)"));
-    problem_expert_->addPredicate(plansys2::Predicate("no_prio_task_remaining"));
+    problem_expert_->addPredicate(plansys2::Predicate("(no_prio_task_remaining"));
     problem_expert_->addPredicate(plansys2::Predicate("(pick_request granny tools)"));
     
   }
@@ -109,7 +109,7 @@ public:
       case STARTING:
         {
           // Set the goal for next state
-          problem_expert_->setGoal(plansys2::Goal("(and(object_at silverware bedroom))"));
+          problem_expert_->setGoal(plansys2::Goal("(and(object_at tools bedroom))"));
 
           // Compute the plan
           auto domain = domain_expert_->getDomain();
